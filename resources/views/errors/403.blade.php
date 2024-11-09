ca171107b9e4af5edd6f82be7ade6526d4392cf1
Jackson Mwesigwa <ssekyanzijacksonmwesigwa@gmail.com>
2024-11-09 12:49:04 +0300

    Create Error Pages

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <section>
+        <x-section-heading>Forbidden</x-section-heading>
+        <div style=" display:flex; margin-top:2rem; justify-content: center; align-items: center;">
+            <h3 style="font-weight: 400">Your Not Authorized to Edit or Delete this Note</h3>
+        </div>
+
+    </section>
+</x-layout>
