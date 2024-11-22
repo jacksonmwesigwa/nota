6512b6bc6f794f1e334de5c5470f0ed992dcc1d4
Jackson Mwesigwa <ssekyanzijacksonmwesigwa@gmail.com>
2024-11-22 13:40:44 +0300

    Favicon not displaying corrected

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Nota - Write, Share, Connect</title>
-    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/svg+xml">
+    <link rel="icon" href="{{ asset('/favicon.ico') }}" type="image/svg+xml">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 
